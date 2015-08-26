567ab8e1643bdf93641ff65c6deff3950060f563
shawetasharma93 <shawetasharma93@yahoo.com>
2015-08-26 23:38:48 +0530

    finally got it

---
+++ Addition/src/sample/Addnumbers.java
@@ -0,0 +1,32 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Addnumbers {
+	
+	
+	public static void main(String[] args) {
+      
+	int a,b;
+      System.out.println("Plzz Enter the variables");
+     
+	Scanner sc = new Scanner(System.in);
+      a=sc.nextInt();
+      b=sc.nextInt();
+      
+      
+      Addnumbers.add(a,b);
+      
+ 
+			
+		}
+
+	private static void add(int a, int b) {
+                  int c = a+b;
+				System.out.println(c);
+                  
+		
+	}
+	}
+
+
